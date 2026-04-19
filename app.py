855119a630f13e12521374d01b8d195506a5810b
avulamani <km757200@gmail.com>
2026-04-19 15:51:51 +0530

    fixed timeout issue

--- app.py
+++ app.py
@@ -166,5 +166,4 @@ def recommend_weather():
 
 if __name__ == '__main__':
     init_models()
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
